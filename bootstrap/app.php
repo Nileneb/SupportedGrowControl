ec978d6ad2a9c359530d42f4833ca5782ab5503b
nileneb <bene.linn@yahoo.de>
2025-12-06 21:49:12 +0100

    feat: Add trust proxies middleware for Nginx reverse proxy support

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Trust all proxies (behind Nginx reverse proxy)
+        $middleware->trustProxies(at: '*');
+        
         $middleware->alias([
             'growdash.webhook' => \App\Http\Middleware\VerifyGrowdashToken::class,
             'device.auth' => \App\Http\Middleware\AuthenticateDevice::class,
